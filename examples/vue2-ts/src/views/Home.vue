89c71abb7d4cce9ba281330b8838233ff22df614
fi3ework <fi3ework@gmail.com>
2021-05-31 22:59:05 +0800

    refactor: add vue2-ts teamplate

---
+++ examples/vue2-ts/src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
+</template>
+
+<script lang="ts">
+export default {}
+</script>
