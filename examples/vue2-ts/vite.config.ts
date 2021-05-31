29de9e2a5c5f303d54baec66d7c00a07053c085f
fi3ework <fi3ework@gmail.com>
2021-06-01 02:16:57 +0800

    feat(vue2): add vti check command

--- examples/vue2-ts/vite.config.ts
+++ examples/vue2-ts/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import { createVuePlugin } from 'vite-plugin-vue2'
 import ViteComponents from 'vite-plugin-components'
+import TsChecker from 'vite-plugin-ts-checker'
+import vlsChecker from 'vite-plugin-ts-checker-preset-vls'
 import { resolve } from 'path'
 
 const config = defineConfig({
@@ -9,15 +11,17 @@ const config = defineConfig({
       '@': `${resolve(__dirname, 'src')}`,
     },
   },
-
   base: '/vue-template/',
-
   build: {
     minify: true,
   },
-
-  plugins: [createVuePlugin({}), ViteComponents({ transformer: 'vue2' })],
-
+  plugins: [
+    createVuePlugin({}),
+    ViteComponents({ transformer: 'vue2' }),
+    TsChecker({
+      checker: vlsChecker(),
+    }),
+  ],
   server: {
     port: 8080,
   },
