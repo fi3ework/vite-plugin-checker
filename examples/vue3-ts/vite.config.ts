798f632d8389f3ec2242f4bc461f79a184c597b1
fi3ework <fi3ework@gmail.com>
2021-06-05 20:49:07 +0800

    fix: add simple vue-tsc

--- examples/vue3-ts/vite.config.ts
+++ examples/vue3-ts/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import TsChecker from 'vite-plugin-ts-checker'
+import Checker from 'vite-plugin-ts-checker'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), TsChecker({ checker: 'vue-tsc' })],
+  plugins: [vue(), Checker({ checker: 'vue-tsc' })],
 })
