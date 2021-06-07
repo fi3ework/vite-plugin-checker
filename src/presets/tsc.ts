e678fdf39011885bc62a166efc85588d6388d3cc
fi3ework <fi3ework@gmail.com>
2021-06-08 00:11:22 +0800

    refactor: clean worker

--- src/presets/tsc.ts
+++ src/presets/tsc.ts
@@ -1,20 +1,13 @@
 import os from 'os'
 import ts from 'typescript'
 import { ErrorPayload } from 'vite'
-import { isMainThread, parentPort, Worker, workerData } from 'worker_threads'
+import { isMainThread, parentPort } from 'worker_threads'
 
-import { ACTION_TYPES } from '../types'
 import { ensureCall, formatHost, tsDiagnosticToViteError } from '../utils'
+import { createScript } from '../worker'
 
-import type {
-  CheckWorker,
-  ConfigAction,
-  ConfigureServerAction,
-  CreateDiagnostic,
-  CheckerFactory,
-  DiagnosticOfCheck,
-  BuildCheckBin,
-} from '../types'
+import type { CreateDiagnostic, ServeCheckerFactory, BuildCheckBin } from '../types'
+
 /**
  * Prints a diagnostic every time the watch status changes.
  * This is mainly for messages like "Starting compilation" or "Compilation completed".
@@ -84,11 +77,6 @@ const createDiagnostic: CreateDiagnostic = (userOptions = {}) => {
                   err: currErr,
                 },
               })
-
-              // server.ws.send({
-              //   type: 'error',
-              //   err: currErr,
-              // })
             }
 
             ensureCall(() => {
@@ -114,53 +102,23 @@ const createDiagnostic: CreateDiagnostic = (userOptions = {}) => {
   }
 }
 
-const checkerFactory: CheckerFactory = () => {
+const checkerFactory: ServeCheckerFactory = () => {
   return {
     createDiagnostic,
   }
 }
+export const buildBin: BuildCheckBin = ['tsc', ['--noEmit']]
+
+const { mainScript, workerScript } = createScript({
+  absFilename: __filename,
+  buildBin,
+  checkerFactory,
+})!
 
 if (isMainThread) {
-  // initialized in main thread
-  const createWorker = (userConfigs?: Record<string, never>): CheckWorker => {
-    const worker = new Worker(__filename, {
-      workerData: userConfigs,
-    })
-
-    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-    return {
-      worker,
-      config: (config) => {
-        const configAction: ConfigAction = { type: ACTION_TYPES.config, payload: config }
-        worker.postMessage(configAction)
-      },
-      configureServer: (serverConfig) => {
-        const configureServerAction: ConfigureServerAction = {
-          type: ACTION_TYPES.configureServer,
-          payload: serverConfig,
-        }
-        worker.postMessage(configureServerAction)
-      },
-    }
-  }
-
+  const { createWorker, serveAndBuild } = mainScript()
   module.exports.createWorker = createWorker
+  module.exports.serveAndBuild = serveAndBuild
 } else {
-  // runs in worker thread
-  let diagnostic: DiagnosticOfCheck | null = null
-  if (!parentPort) throw Error('should have parentPort as file runs in worker thread')
-
-  parentPort.on('message', (action: ConfigAction | ConfigureServerAction) => {
-    if (action.type === ACTION_TYPES.config) {
-      const checker = checkerFactory()
-      const userConfigs = workerData
-      diagnostic = checker.createDiagnostic(userConfigs)
-      diagnostic.config(action.payload)
-    } else if (action.type === ACTION_TYPES.configureServer) {
-      if (!diagnostic) throw Error('diagnostic should be initialized in `config` hook of Vite')
-      diagnostic.configureServer(action.payload)
-    }
-  })
+  workerScript()
 }
-
-export const buildBin: BuildCheckBin = ['tsc', ['--noEmit']]
