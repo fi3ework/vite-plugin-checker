836ab02aa6f196c265e6e0df6f9f75acbe17d03c
fi3ework <fi3ework@gmail.com>
2021-07-13 03:22:01 +0800

    test: transfrom to relative path in win32

--- scripts/logTimeSerializers.ts
+++ scripts/logTimeSerializers.ts
@@ -2,6 +2,8 @@
 
 const readyTimeReg = /ready in \d+ms\./im
 const hmrUpdateTimeReg = /\d+:\d+:\d+ [AP]M /im
+const winPathReg = /(D:)?\/.?\/vite-plugin-checker\/vite-plugin-checker/im
+import os from 'os'
 
 /**
  * erase log time
@@ -12,12 +14,16 @@ const logTimeSerializers = {
     let result = val
     result = result.replace(readyTimeReg, 'ready in XXXms')
     result = result.replace(hmrUpdateTimeReg, 'HH:MM:SS AM ')
+    if (os.platform() === 'win32') {
+      result = result.replace(winPathReg, '<PROJECT_ROOT>')
+    }
     return serialize(result)
   },
   test(val: any) {
     if (typeof val !== 'string') return false
     if (readyTimeReg.test(val)) return true
     if (hmrUpdateTimeReg.test(val)) return true
+    if (os.platform() === 'win32' && winPathReg.test(val)) return true
     return false
   },
 }
