bab6a68f44a3be9550ac0082866906e3dfa056fa
fi3ework <fi3ework@gmail.com>
2022-01-18 03:33:44 +0800

    test: add test for vls config

---
+++ packages/vite-plugin-checker/__tests__/unit/vlsConfig.spec.ts
@@ -0,0 +1,47 @@
+import path from 'path'
+import { ShutdownRequest } from 'vscode-languageserver/node'
+import { URI } from 'vscode-uri'
+
+import { prepareClientConnection } from '../../src/checkers/vls/diagnostics'
+
+async function testVslConfig(overrideConfig?: any) {
+  const workspaceUri = URI.file(path.join(__dirname, 'fixtures'))
+  const { clientConnection, serverConnection, vls, up, down, logger } =
+    await prepareClientConnection(workspaceUri, {
+      watch: false,
+      verbose: false,
+      config: overrideConfig || null,
+    })
+
+  // @ts-expect-error
+  expect(vls.workspaceConfig).toMatchSnapshot()
+
+  // TODO: this test case will let Jest hang with out --forceExit option
+  // current enable forceExit but we should find which resource is not released
+  await clientConnection.sendRequest(ShutdownRequest.type)
+  clientConnection.dispose()
+  clientConnection.end()
+  serverConnection.dispose()
+  up.destroy()
+  down.destroy()
+  vls.dispose()
+}
+
+describe('VLS config', () => {
+  it('default config', async () => {
+    await testVslConfig()
+  })
+
+  it('customized config', async () => {
+    await testVslConfig({
+      vetur: {
+        validation: {
+          template: false,
+          templateProps: false,
+          interpolation: false,
+          style: false,
+        },
+      },
+    })
+  })
+})
