5843e7c75c92959f4c9bd39500525a733e34e5c4
fi3ework <fi3ework@gmail.com>
2022-01-23 19:09:15 +0800

    feat: support show multiple errors

--- packages/vite-plugin-checker/src/@runtime/main.ts
+++ packages/vite-plugin-checker/src/@runtime/main.ts
@@ -1,24 +1,24 @@
 import type { ErrorPayload } from 'vite'
 import { ErrorOverlay, overlayId } from './overlay'
-
 let enableOverlay = true
 
+const WS_CHECKER_ERROR_TYPE = 'vite-plugin-checker-error'
 export function inject() {
   const socketProtocol = null || (location.protocol === 'https:' ? 'wss' : 'ws')
   const socketHost = `${null || location.hostname}:${'3000'}`
   const socket = new WebSocket(`${socketProtocol}://${socketHost}`, 'vite-hmr')
+
   socket.addEventListener('message', async ({ data: dataStr }) => {
     const data = JSON.parse(dataStr)
-    if (data.type === 'checker-error') {
-      createErrorOverlay(data.err)
+    if (data.type === WS_CHECKER_ERROR_TYPE) {
+      createErrorOverlay([data.err])
     }
   })
 }
 
-function createErrorOverlay(err: ErrorPayload['err']) {
+function createErrorOverlay(err: ErrorPayload['err'][]) {
   if (!enableOverlay) return
   clearErrorOverlay()
-  console.log('error! 标记!')
   document.body.appendChild(new ErrorOverlay(err))
 }
 
