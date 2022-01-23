953339fe3efaeae82f6fe3e234da05d81f0e4ca7
fi3ework <fi3ework@gmail.com>
2022-01-23 19:09:15 +0800

    feat: support show multiple errors

--- packages/vite-plugin-checker/src/client/index.ts
+++ packages/vite-plugin-checker/src/client/index.ts
@@ -1,10 +1,9 @@
-const RUNTIME_PUBLIC_PATH = '/@vite-plugin-checker/runtime'
 import fs from 'fs'
 
-const runtimeFilePath = require.resolve('../@runtime/main.js')
+const RUNTIME_PUBLIC_PATH = '/@vite-plugin-checker-runtime'
+const RUNTIME_FILE_PATH = require.resolve('../@runtime/main.js')
+const WS_CHECKER_ERROR_TYPE = 'vite-plugin-checker-error'
 
-const runtimeCode = `
-${fs.readFileSync(runtimeFilePath, 'utf-8')};
-`
+const runtimeCode = `${fs.readFileSync(RUNTIME_FILE_PATH, 'utf-8')};`
 
-export { runtimeCode, RUNTIME_PUBLIC_PATH }
+export { runtimeCode, RUNTIME_PUBLIC_PATH, WS_CHECKER_ERROR_TYPE }
