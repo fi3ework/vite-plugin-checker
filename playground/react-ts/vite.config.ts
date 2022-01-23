e5a5314951111b36c07eb80cbd920fcf6821cb91
fi3ework <fi3ework@gmail.com>
2022-01-23 22:50:04 +0800

    refactor: bump vite and plugin versoin

--- playground/react-ts/vite.config.ts
+++ playground/react-ts/vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import react from '@vitejs/plugin-react'
 import checker from 'vite-plugin-checker'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    reactRefresh(),
+    react(),
     checker({
       typescript: true,
     }),
