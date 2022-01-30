6960f3d092a0d0c88afa39dcee78dc2459d189d5
fi3ework <fi3ework@gmail.com>
2022-01-30 22:23:14 +0800

    feat: use Svelte to write runtime code

---
+++ packages/runtime/src/components/Checker.svelte
@@ -0,0 +1,20 @@
+<script>
+  import Diagnostic from './Diagnostic.svelte'
+  export let diagnostics
+</script>
+
+<ul>
+  {#each diagnostics as diagnostic}
+    <Diagnostic {diagnostic} />
+  {/each}
+</ul>
+
+<style>
+  ul {
+    list-style: none;
+  }
+  ul {
+    padding-inline: 0;
+    margin-block: 0;
+  }
+</style>
---
+++ packages/runtime/src/components/Diagnostic.svelte
@@ -0,0 +1,119 @@
+<script>
+  export let diagnostic
+
+  const fileRE = /(?:[a-zA-Z]:\\|\/).*?:\d+:\d+/g
+  const codeframeRE = /^(?:>?\s+\d+\s+\|.*|\s+\|\s*\^.*)\r?\n/gm
+  codeframeRE.lastIndex = 0
+  $: hasFrame = diagnostic.frame && codeframeRE.test(diagnostic.frame)
+  $: message = hasFrame ? diagnostic.message.replace(codeframeRE, '') : diagnostic.message
+  // TODO: stackLinks not used
+  $: stackLinks = calcLink(diagnostic.stack)
+  $: [file] = (diagnostic.loc?.file || err.id || 'unknown file').split(`?`)
+
+  $: errorSource = diagnostic.loc
+    ? { ...calcLink(`${file}:${diagnostic.loc.line}:${diagnostic.loc.column}`)[0], linkFiles: true }
+    : { text: file, linkFiles: false }
+
+  function calcLink(text) {
+    let curIndex = 0
+    let match
+    const links = []
+    while ((match = fileRE.exec(text))) {
+      const { 0: file, index } = match
+      if (index !== null) {
+        const frag = text.slice(curIndex, index)
+        const link = {}
+        link.textContent = file
+        link.onclick = () => {
+          fetch('/__open-in-editor?file=' + encodeURIComponent(file))
+        }
+        curIndex += frag.length + file.length
+        links.push(link)
+      }
+    }
+
+    return links
+  }
+</script>
+
+<li class="message-item">
+  <pre class="message">
+    <span class="plugin">{`[${diagnostic.plugin}] `}</span>
+    <span class="message-body">{message}</span>
+  </pre>
+  <pre class="file">
+    {#if errorSource.linkFiles}
+      <!-- svelte-ignore a11y-missing-attribute -->
+      <a
+        class="file-link"
+        on:click={errorSource.onclick}>{errorSource.textContent}</a
+      >
+    {:else}
+      {errorSource.text}
+    {/if}
+  </pre>
+  {#if hasFrame}
+    <pre class="frame">{diagnostic.frame.trim()}</pre>
+  {/if}
+  <pre class="stack">{diagnostic.stack}</pre>
+</li>
+
+<style>
+  li {
+    list-style: none;
+  }
+
+  .message-item {
+    border-top: 1px dotted #666;
+    padding: 12px 0 0 0;
+  }
+
+  .message {
+    line-height: 1.3;
+    font-weight: 600;
+    white-space: initial;
+  }
+
+  pre {
+    font-family: var(--monospace);
+    font-size: 14px;
+    margin-top: 0;
+    margin-bottom: 0;
+    overflow-x: scroll;
+    scrollbar-width: none;
+  }
+
+  pre::-webkit-scrollbar {
+    display: none;
+  }
+
+  .message-body {
+    color: var(--red);
+  }
+
+  .plugin {
+    color: var(--purple);
+  }
+
+  .file {
+    color: var(--cyan);
+    margin-bottom: 0;
+    white-space: pre-wrap;
+    word-break: break-all;
+  }
+
+  .frame {
+    margin: 1em 0;
+    color: var(--yellow);
+  }
+
+  .stack {
+    font-size: 13px;
+    color: var(--dim);
+  }
+
+  .file-link {
+    text-decoration: underline;
+    cursor: pointer;
+  }
+</style>
