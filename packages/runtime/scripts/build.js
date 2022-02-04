d9bd1069a5cfd0b87d77ba267db4da92540091ad
fi3ework <fi3ework@gmail.com>
2022-02-04 23:43:52 +0800

    feat: rewrite overlay

--- packages/runtime/scripts/build.js
+++ packages/runtime/scripts/build.js
@@ -3,14 +3,25 @@ import sveltePlugin from 'esbuild-svelte'
 import hasFlag from 'has-flag'
 
 const isWatch = hasFlag('watch')
+const isLocal = hasFlag('local')
+
+const config = isLocal
+  ? {
+      entryPoints: ['./src/main-local.js'],
+      outfile: './public/build/bundle.js',
+    }
+  : {
+      entryPoints: ['./src/main.js'],
+      outfile: '../vite-plugin-checker/lib/@runtime/main.js',
+    }
 
 esbuild
   .build({
-    entryPoints: ['./src/main.js'],
+    entryPoints: config.entryPoints,
     bundle: true,
     format: 'esm',
     watch: isWatch,
-    outfile: '../vite-plugin-checker/lib/@runtime/main.js',
+    outfile: config.outfile,
     plugins: [sveltePlugin({ compilerOptions: { css: true } })],
     logLevel: 'info',
   })
