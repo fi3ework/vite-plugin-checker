507e4cb50ac72d79456159d3ac2422e92fa581b4
fi3ework <fi3ework@gmail.com>
2022-02-08 11:21:36 +0800

    fix: remove media query

--- packages/runtime/src/App.svelte
+++ packages/runtime/src/App.svelte
@@ -54,20 +54,6 @@
     opacity: 1;
     pointer-events: all;
     transform: translateY(0px) scale(1);
-    /* font-family: var(--monospace);
-    line-height: 1.5;
-    width: 800px;
-    color: #d8d8d8;
-    margin: 40px auto;
-    padding: 16px 32px 32px;
-    position: relative;
-    background: #24272e;
-    border-radius: 6px 6px 8px 8px;
-    box-shadow: 0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.22);
-    overflow: scroll;
-    direction: ltr;
-    text-align: left;
-    max-height: 80vh; */
   }
 
   .window-collapsed {
@@ -86,17 +72,8 @@
   }
 
   main {
-    /* text-align: center; */
     padding: 16px;
-    max-width: 240px;
     width: 100%;
     box-sizing: border-box;
-    /* margin: 0 auto; */
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
   }
 </style>
