998303eaeef6699eaae5ba2a7643a2dee577a0b8
Volodymyr Zhdanov <wight554@gmail.com>
2022-07-22 19:12:41 +0300

    remove extra semicolon

--- packages/vite-plugin-checker/src/main.ts
+++ packages/vite-plugin-checker/src/main.ts
@@ -260,4 +260,4 @@ export function isObject(value: unknown): value is Record<string, any> {
   return Object.prototype.toString.call(value) === '[object Object]'
 }
 
-export default checker;
+export default checker
