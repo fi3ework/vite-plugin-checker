602d446ea0435ec573c103e9d96a6619a5bb50e1
fi3ework <fi3ework@gmail.com>
2022-08-27 00:50:33 +0800

    chore: remove unnecessary export

--- packages/vite-plugin-checker/src/main.ts
+++ packages/vite-plugin-checker/src/main.ts
@@ -266,7 +266,7 @@ function spawnChecker(
   })
 }
 
-export function isObject(value: unknown): value is Record<string, any> {
+function isObject(value: unknown): value is Record<string, any> {
   return Object.prototype.toString.call(value) === '[object Object]'
 }
 
