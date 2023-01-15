4ae29b4acf08b336063e6a42aae1f4505ae60eb0
fi3ework <fi3ework@gmail.com>
2023-01-15 15:37:29 +0800

    build: only deploy docs when tagging a commit

--- scripts/docs-check.sh
+++ scripts/docs-check.sh
@@ -1,6 +1,11 @@
 echo "prev commit: $CACHED_COMMIT_REF"
 echo "current commit: $COMMIT_REF"
 git diff --quiet $CACHED_COMMIT_REF $COMMIT_REF docs package.json pnpm-lock.yaml netlify.toml scripts/docs-check.sh
-status=$?
-echo "diff exit code: $status"
-exit $status
+has_diff=$?
+git describe --exact-match --tags $(git log -n1 --pretty='%h')
+has_tag=$?
+
+echo "diff exit code: $has_diff"
+echo "tag exit code: $has_tag"
+
+exit $(($has_diff || $has_tag))
