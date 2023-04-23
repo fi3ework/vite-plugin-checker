63b65fa57b224523d9d49179c92e0d12db25e8b3
fi3ework <fi3ework@gmail.com>
2023-04-23 22:16:59 +0800

    build: bump vitest

--- packages/vite-plugin-checker/src/utils.ts
+++ packages/vite-plugin-checker/src/utils.ts
@@ -1,5 +1,5 @@
 import { isMainThread as _isMainThread, threadId } from 'worker_threads'
 
 // since vitest run all cases in worker thread, we should compatible with it to pass E2E tests
-export const isInVitestEntryThread = threadId === 1 && process.env['VITEST']
+export const isInVitestEntryThread = threadId === 0 && process.env['VITEST']
 export const isMainThread = _isMainThread || isInVitestEntryThread
