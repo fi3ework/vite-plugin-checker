c751edfd48e3eb620bedfce7b4a0e9da81b302f7
fi3ework <fi3ework@gmail.com>
2023-07-08 17:40:42 +0800

    fix: panelStyle not work in runtime

--- playground/config-overlay-position-style/vite.config.js
+++ playground/config-overlay-position-style/vite.config.js
@@ -4,7 +4,11 @@ import checker from 'vite-plugin-checker'
 export default defineConfig({
   plugins: [
     checker({
-      overlay: { position: 'tr', badgeStyle: 'background-color: #E799B0' },
+      overlay: {
+        position: 'tr',
+        badgeStyle: 'background-color: #E799B0',
+        panelStyle: 'background-color: #A4C1FF;',
+      },
       eslint: {
         lintCommand: 'eslint ./src --ext .ts',
       },
