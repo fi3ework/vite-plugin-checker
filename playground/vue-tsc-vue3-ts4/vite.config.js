0ad1c91975586d43e2f38a4da1b7fca4a5082726
fi3ework <fi3ework@gmail.com>
2024-02-02 22:33:11 +0800

    remove useless test

--- playground/vue-tsc-vue3-ts4/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { checker } from 'vite-plugin-checker'
-import { checker as vueTsc } from '@vite-plugin-checker/vue-tsc'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), checker([vueTsc()])],
-})
