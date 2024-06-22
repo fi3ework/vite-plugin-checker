087b5762dab23c685f17b860036dc2e93adcbce0
Ray <i@mk1.io>
2024-06-22 18:01:54 +0800

    Update tsup.config.ts

--- packages/vite-plugin-checker/tsup.config.ts
+++ packages/vite-plugin-checker/tsup.config.ts
@@ -27,12 +27,6 @@ export default defineConfig([
         'dist/cjs/checkers/vueTsc/languagePlugins.js',
         'dist/cjs/checkers/vueTsc/languagePlugins.cjs'
       )
-      try {
-        await copyFile(
-          'dist/cjs/checkers/vueTsc/languagePlugins.js.map',
-          'dist/cjs/checkers/vueTsc/languagePlugins.cjs.map'
-        )
-      } catch (e) {}
     },
     ...shared,
   },
@@ -41,17 +35,10 @@ export default defineConfig([
     outDir: 'dist/esm',
     async onSuccess() {
       await rm('dist/esm/checkers/vueTsc/languagePlugins.js')
-      await rm('dist/esm/checkers/vueTsc/languagePlugins.js.map')
       await copyFile(
         'dist/cjs/checkers/vueTsc/languagePlugins.cjs',
         'dist/esm/checkers/vueTsc/languagePlugins.cjs'
       )
-      try {
-        await copyFile(
-          'dist/cjs/checkers/vueTsc/languagePlugins.cjs.map',
-          'dist/esm/checkers/vueTsc/languagePlugins.cjs.map'
-        )
-      } catch (e) {}
     },
     ...shared,
   },
