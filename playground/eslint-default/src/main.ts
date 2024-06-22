842a938cb07737906b402dad1ae8d9b0c6185a53
so1ve <i@mk1.io>
2024-06-22 22:55:26 +0800

    test: update test

--- playground/eslint-default/src/main.ts
+++ playground/eslint-default/src/main.ts
@@ -1,5 +1,6 @@
 import { text } from './text'
 
+let count: string = 0
 var hello = 'Hello'
 
 const rootDom = document.querySelector('#root')!
--- playground/eslint-default/vite.config.js
+++ playground/eslint-default/vite.config.js
@@ -4,6 +4,7 @@ import checker from 'vite-plugin-checker'
 export default defineConfig({
   plugins: [
     checker({
+      typescript: true,
       eslint: {
         lintCommand: 'eslint ./src --ext .ts',
         // dev: { logLevel: ['warning'] },
