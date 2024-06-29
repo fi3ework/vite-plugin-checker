6290de84e159d3df8879b32581a551352cc69d66
fi3ework <fi3ework@gmail.com>
2024-06-30 03:37:51 +0800

    test: correct test cases

--- playground/config-initialIsOpen-error-warnings/__tests__/test.spec.ts
+++ playground/config-initialIsOpen-error-warnings/__tests__/test.spec.ts
@@ -1,31 +1,34 @@
 import { describe, expect, it } from 'vitest'
 
-import { editFile, getHmrOverlayText, isServe, sleepForEdit, sleepForServerReady } from '../../testUtils'
+import {
+  editFile,
+  getHmrOverlay,
+  getHmrOverlayText,
+  isServe,
+  pollingUntil,
+  sleepForEdit,
+  sleepForServerReady,
+} from '../../testUtils'
 
 describe('config-initialIsOpen-error-warnings', () => {
   describe.runIf(isServe)('serve', () => {
-    it('should not find overlay', async () => {
-      if (isServe) {
-        await sleepForServerReady()
-        try {
-          await getHmrOverlayText()
-        } catch (e) {
-          expect((e as any).toString()).toContain(
-            'Invariant failed: <vite-plugin-checker-error-overlay> shadow dom is expected to be found, but got null'
-          )
-        }
-
-        console.log('-- overlay remains closed after introduced error --')
-        editFile('src/main.ts', (code) => code.replace('! as', '! is'))
-        await sleepForEdit()
-        try {
-          await getHmrOverlayText()
-        } catch (e) {
-          expect((e as any).toString()).toContain(
-            'Invariant failed: <vite-plugin-checker-error-overlay> shadow dom is expected to be found, but got null'
-          )
-        }
+    it('should not show overlay when only warning exists', async () => {
+      await sleepForServerReady()
+      try {
+        await getHmrOverlayText()
+      } catch (e) {
+        expect((e as any).toString()).toContain(
+          'Invariant failed: <vite-plugin-checker-error-overlay> shadow dom is expected to be found, but got null'
+        )
       }
+
+      console.log('-- overlay appears after introduce an error --')
+      editFile('src/main.ts', (code) => code.replace('const rootDom', 'var rootDom'))
+      await sleepForEdit()
+      await getHmrOverlayText()
+      await pollingUntil(getHmrOverlay, (dom) => !!dom)
+      const [, , frame] = await getHmrOverlayText()
+      expect(frame).toMatchSnapshot()
     })
   })
 })
