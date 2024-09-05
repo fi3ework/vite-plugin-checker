5f5b92a91401bfa59417c71d1048e7041aa03881
Danila Rodichkin <daniluk4000@outlook.com>
2024-09-05 20:35:32 +0300

    fix(vue-tsc): updated createVueLanguagePlugin syntax, version pin (#398)
    * fix(vue-tsc): compatibility with new createVueLanguagePlugin syntax, version pin
    
    * fix: TS issues
    
    * fix: TS issues
    
    * chore: pin language core
    
    * chore: upgrade to vue-tsc 2.1.6
    
    ---------
    
    Co-authored-by: drodichkin <drodichkin@azbukavkusa.ru>

--- packages/vite-plugin-checker/src/checkers/vueTsc/languagePlugins.cjs
+++ packages/vite-plugin-checker/src/checkers/vueTsc/languagePlugins.cjs
@@ -1,11 +1,20 @@
 const path = require('node:path')
-const { removeEmitGlobalTypes } = require('vue-tsc')
 
 const vueTscDir = path.dirname(require.resolve('vue-tsc/package.json'))
 const vue = /** @type {typeof import('@vue/language-core')} */ (
   require(require.resolve('@vue/language-core', { paths: [vueTscDir] }))
 )
 const windowsPathReg = /\\/g
+
+const removeEmitGlobalTypesRegexp = /^[^\n]*__VLS_globalTypesStart[\w\W]*__VLS_globalTypesEnd[^\n]*\n?$/mg;
+
+/**
+ * @param dts {string}
+ * @returns {string}
+ */
+function removeEmitGlobalTypes(dts) {
+  return dts.replace(removeEmitGlobalTypesRegexp, '');
+}
 
 // #region copied from https://github.com/vuejs/language-tools/blob/0781998a29f176ad52c30d3139d5c78a5688bd5d/packages/tsc/index.ts
 /**
@@ -29,21 +38,9 @@ exports.getLanguagePlugins = (ts, options) => {
   }
   const vueLanguagePlugin = vue.createVueLanguagePlugin(
     ts,
-    (id) => id,
-    () => '',
-    (fileName) => {
-      const fileMap = new vue.FileMap(
-        host?.useCaseSensitiveFileNames?.() ?? false,
-      )
-      for (const vueFileName of options.rootNames.map((rootName) =>
-        rootName.replace(windowsPathReg, '/'),
-      )) {
-        fileMap.set(vueFileName, undefined)
-      }
-      return fileMap.has(fileName)
-    },
     options.options,
     vueOptions,
+    (id) => id,
   )
   return [vueLanguagePlugin]
 }
