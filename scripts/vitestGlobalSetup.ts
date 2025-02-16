62b51a9cd1c357061fe361d40f0d2efa61365940
Daniel Roe <daniel@roe.dev>
2025-02-16 20:30:58 +0000

    perf: drop `fs-extra` in favour of native `node:fs`

--- scripts/vitestGlobalSetup.ts
+++ scripts/vitestGlobalSetup.ts
@@ -1,5 +1,5 @@
 import path from 'node:path'
-import fs from 'fs-extra'
+import fs from 'node:fs/promises'
 
 const tempRuntimePath = path.resolve(
   __dirname,
@@ -7,17 +7,18 @@ const tempRuntimePath = path.resolve(
 )
 
 export async function setup(): Promise<void> {
-  await fs.ensureDir(tempRuntimePath)
-  await fs.emptyDir(tempRuntimePath)
-  await fs.copy(
+  await fs.rm(tempRuntimePath, { force: true, recursive: true })
+  await fs.mkdir(tempRuntimePath, { recursive: true })
+  await fs.cp(
     path.resolve(__dirname, '../packages/vite-plugin-checker/dist/@runtime'),
     tempRuntimePath,
     {
+      recursive: true,
       dereference: false,
     },
   )
 }
 
 export async function teardown(): Promise<void> {
-  fs.removeSync(tempRuntimePath)
+  fs.rm(tempRuntimePath, { force: true, recursive: true })
 }
