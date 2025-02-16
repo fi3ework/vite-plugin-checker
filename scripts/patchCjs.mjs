ecd015f413795c13ec89f3808a0ffc7b1e677b9d
Daniel Roe <daniel@roe.dev>
2025-02-16 20:33:58 +0000

    chore: lint

--- scripts/patchCjs.mjs
+++ scripts/patchCjs.mjs
@@ -1,6 +1,6 @@
+import fs from 'node:fs/promises'
 import path, { dirname } from 'node:path'
 import { fileURLToPath } from 'node:url'
-import fs from 'node:fs/promises'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
