339df99a864ef1572210128b91386ca26f158a93
Daniel Roe <daniel@roe.dev>
2025-02-18 07:34:22 +0100

    fix: normalise path separators

--- packages/vite-plugin-checker/src/glob.ts
+++ packages/vite-plugin-checker/src/glob.ts
@@ -6,8 +6,8 @@ export function createIgnore(root: string, pattern: string | string[] = []) {
   const paths = Array.isArray(pattern) ? pattern : [pattern]
 
   const globs = paths.flatMap((f) => {
-    const resolvedPath = resolve(root, f)
-    const relativePath = relative(root, resolve(root, f))
+    const resolvedPath = resolve(root, f).replace(/\\/g, '/')
+    const relativePath = relative(root, resolvedPath)
     try {
       const isDirectory =
         !relativePath.includes('*') && statSync(resolvedPath).isDirectory()
@@ -18,9 +18,12 @@ export function createIgnore(root: string, pattern: string | string[] = []) {
     return [relativePath]
   })
 
-  return (path: string, _stats?: Stats) =>
-    path.includes('node_modules') ||
-    (path !== root &&
-      !zeptomatch(globs, relative(root, path)) &&
-      !(_stats ?? statSync(path)).isDirectory())
+  return (path: string, _stats?: Stats) => {
+    return (
+      path.includes('node_modules') ||
+      (path !== root &&
+        !zeptomatch(globs, relative(root, path).replace(/\\/g, '/')) &&
+        !(_stats ?? statSync(path)).isDirectory())
+    )
+  }
 }
