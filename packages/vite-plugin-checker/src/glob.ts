664ce4cfbf6e0a29a2ebe4dd9b68e64cc0269755
Daniel Roe <daniel@roe.dev>
2025-02-18 09:28:19 +0100

    fix: more normalisation

--- packages/vite-plugin-checker/src/glob.ts
+++ packages/vite-plugin-checker/src/glob.ts
@@ -2,12 +2,13 @@ import { type Stats, statSync } from 'node:fs'
 import { join, relative, resolve } from 'node:path'
 import zeptomatch from 'zeptomatch'
 
-export function createIgnore(root: string, pattern: string | string[] = []) {
+export function createIgnore(_root: string, pattern: string | string[] = []) {
   const paths = Array.isArray(pattern) ? pattern : [pattern]
+  const root = _root.replace(/\\/g, '/')
 
   const globs = paths.flatMap((f) => {
     const resolvedPath = resolve(root, f).replace(/\\/g, '/')
-    const relativePath = relative(root, resolvedPath)
+    const relativePath = relative(root, resolvedPath).replace(/\\/g, '/')
     try {
       const isDirectory =
         !relativePath.includes('*') && statSync(resolvedPath).isDirectory()
