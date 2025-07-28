badb8fb5c329e7b4d818b1b2ccb9b77eed266018
KazariEX <zinkawaii@qq.com>
2025-07-28 22:13:23 +0800

    refactor(vueTsc): adaptation of vue-tsc@3.0.3

--- packages/vite-plugin-checker/src/checkers/vueTsc/languagePlugins.cjs
+++ packages/vite-plugin-checker/src/checkers/vueTsc/languagePlugins.cjs
@@ -2,24 +2,10 @@ const path = require('node:path')
 
 const vueTscDir = path.dirname(require.resolve('vue-tsc/package.json'))
 const vue =
-  /** @type {typeof import('@vue/language-core') & { resolveVueCompilerOptions?: any }} */ (
+  /** @type {typeof import('@vue/language-core')} */ (
     require(require.resolve('@vue/language-core', { paths: [vueTscDir] }))
   )
 const windowsPathReg = /\\/g
-
-const removeEmitGlobalTypesRegexp =
-  /^[^\n]*__VLS_globalTypesStart[\w\W]*__VLS_globalTypesEnd[^\n]*\n?$/gm
-
-/**
- * @param dts {string}
- * @returns {string}
- */
-function removeEmitGlobalTypes(dts) {
-  return dts.replace(removeEmitGlobalTypesRegexp, '')
-}
-
-const getDefaultCompilerOptions =
-  vue.getDefaultCompilerOptions || (() => vue.resolveVueCompilerOptions({}))
 
 // #region copied from https://github.com/vuejs/language-tools/blob/0781998a29f176ad52c30d3139d5c78a5688bd5d/packages/tsc/index.ts
 /**
@@ -35,12 +21,8 @@ exports.getLanguagePlugins = (ts, options) => {
           ts.sys,
           configFilePath.replace(windowsPathReg, '/'),
         ).vueOptions
-      : getDefaultCompilerOptions()
-  const host = /** @type {import('typescript').CompilerHost} */ (options.host)
-  const writeFile = host.writeFile.bind(host)
-  host.writeFile = (fileName, contents, ...args) => {
-    return writeFile(fileName, removeEmitGlobalTypes(contents), ...args)
-  }
+      : vue.getDefaultCompilerOptions()
+  vue.writeGlobalTypes(vueOptions, ts.sys.writeFile)
   const vueLanguagePlugin = vue.createVueLanguagePlugin(
     ts,
     options.options,
